54bbc5bc5fdb5a209369d2096193f41cbf68d2f7
ramc9777 <ramc9777@gmail.com>
2022-07-29 06:57:55 +0530

    For  Loop addition

---
+++ Lecture/loops/README.md
@@ -0,0 +1,9 @@
+# What are loops?
+- Loops are used to execute a set of instructions for a fixed number of times.
+## Types of Loops
+- for-loop
+```
+for(init; cond; increment/decrement){
+    code;
+}
+```
---
+++ Lecture/loops/loops.java
@@ -0,0 +1,13 @@
+package lecture.loops;
+
+public class loops {
+    static void numberLoop(int x) {
+        for (int i = 1; i <= x; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        numberLoop(6);
+    }
+}
